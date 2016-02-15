8642e4c919913ca626c4efd963b23972aaafda56
ragone <ragonedk@gmail.com>
2016-02-16 11:10:06 +1300

    audio added

--- app/src/main/java/ragone/io/quietmind/MainActivity.java
+++ app/src/main/java/ragone/io/quietmind/MainActivity.java
@@ -1,38 +1,54 @@
 package ragone.io.quietmind;
 
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.lantouzi.wheelview.WheelView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView monday;
     private WheelView wheelView;
     private CountDownTimer timer;
     private int selectedTime;
     private SwitchCompat bellsSwitch;
+    private List<ImageView> days;
+    private int currentDay;
+    private float brightness;
+    private SoundPool soundPool;
+    private int sound;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bellsSwitch = (SwitchCompat) findViewById(R.id.enableBells);
+        days = new ArrayList<>();
+        days.add((ImageView) findViewById(R.id.sunday_check));
+        days.add((ImageView) findViewById(R.id.monday_check));
+        days.add((ImageView) findViewById(R.id.tuesday_check));
+        days.add((ImageView) findViewById(R.id.wednesday_check));
+        days.add((ImageView) findViewById(R.id.thursday_check));
+        days.add((ImageView) findViewById(R.id.friday_check));
+        days.add((ImageView) findViewById(R.id.saturday_check));
+
+        Calendar calendar = Calendar.getInstance();
+        currentDay = calendar.get(Calendar.DAY_OF_WEEK);
+
+//        bellsSwitch = (SwitchCompat) findViewById(R.id.enableBells);
         final PlayPauseView view = (PlayPauseView) findViewById(R.id.play_pause_view);
         view.toggle();
         view.setOnClickListener(new View.OnClickListener() {
@@ -40,35 +56,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 view.toggle();
                 if(view.getDrawable().isPlay()) {
-                    wheelView.setEnabled(false);
-                    bellsSwitch.setEnabled(false);
+                    setInputFieldEnabled(false);
+                    MediaPlayer.create(MainActivity.this, R.raw.bell2).start();
                     selectedTime = wheelView.getSelectedPosition() + 1;
                     timer = new CountDownTimer(selectedTime * 60000, 1000) {
                         @Override
                         public void onTick(long millisUntilFinished) {
-                            wheelView.smoothSelectIndex((int) Math.ceil(millisUntilFinished / 60000));
-                            Log.v("Timer", String.valueOf(millisUntilFinished));
+                            int timeLeftInMinutes = (int) Math.ceil(millisUntilFinished / 60000);
+                            wheelView.smoothSelectIndex(timeLeftInMinutes);
                         }
 
                         @Override
                         public void onFinish() {
+                            MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bell1);
+                            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                                int maxCount = 2;
+
+                                @Override
+                                public void onCompletion(MediaPlayer mediaPlayer) {
+                                    if (count < maxCount) {
+                                        count++;
+                                        mediaPlayer.seekTo(0);
+                                        mediaPlayer.start();
+                                    } else {
+                                        mediaPlayer.release();
+                                        count = 0;
+                                    }
+                                }
+                            });
+                            mediaPlayer.start();
                             view.toggle();
-                            bellsSwitch.setEnabled(true);
+                            setInputFieldEnabled(true);
                             wheelView.smoothSelectIndex(selectedTime - 1);
+                            days.get(currentDay - 1).setImageResource(R.drawable.check_green);
                         }
                     }.start();
                 } else {
                     timer.cancel();
-                    wheelView.setEnabled(true);
-                    bellsSwitch.setEnabled(true);
+                    setInputFieldEnabled(true);
                     wheelView.smoothSelectIndex(selectedTime - 1);
                 }
             }
         });
-
-        monday = (ImageView) findViewById(R.id.monday_check);
-        monday.setImageResource(R.drawable.check_red);
-
         wheelView = (WheelView) findViewById(R.id.wheel);
         List<String> data = new LinkedList<>();
         for(int i = 1; i <= 60; i++) {
@@ -78,25 +107,8 @@ public class MainActivity extends AppCompatActivity {
         wheelView.selectIndex(14);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void setInputFieldEnabled(boolean isEnabled) {
+//        bellsSwitch.setEnabled(isEnabled);
+        wheelView.setEnabled(isEnabled);
     }
 }
